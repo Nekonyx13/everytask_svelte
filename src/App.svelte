ecffe12cb8f8dea2ca3d24daa9b5b83cf3525f9e
Nekonyx13 <corvin.prath@gmail.com>
2022-03-06 11:37:28 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,156 @@
+<script>
+	import Navbar from "./lib/components/Navbar.svelte";
+</script>
+
+<main>
+	<h1>Hello Gaming!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+
+	<Navbar />
+</main>
+
+<style lang="scss">
+	main {
+		min-height: 100%;
+		background-color: #f5fbfe;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+		margin: 0;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: 640px;
+			margin: auto;
+			border: 1px solid black;
+		}
+	}
+</style>
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,59 @@
+<script>
+    import { CheckCircle, Sheet, Users, Settings } from "lucide-svelte";
+</script>
+
+<nav class="nav-bar">
+    <div class="nav-item">
+        <CheckCircle size="35" strokeWidth="2" />
+    </div>
+    <div class="nav-item">
+        <Sheet size="35" strokeWidth="2" />
+    </div>
+    <div class="nav-item">
+        <Users size="35" strokeWidth="2" />
+    </div>
+    <div class="nav-item">
+        <Settings size="35" strokeWidth="2" />
+    </div>
+</nav>
+
+<style lang="scss">
+    .nav-bar {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        height: 5rem;
+
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        border-top-left-radius: 1em;
+        border-top-right-radius: 1em;
+        background-color: #f5fbfe;
+        box-shadow: 0px -28px 27px 20px #f5fbfe;
+        padding-bottom: 1em;
+        padding-top: 1em;
+
+        .nav-item {
+            color: #5c2372;
+            font-size: 0px;
+            background: #f5fbfe;
+            border-radius: 1rem;
+        
+            padding: 20px;
+
+            transition: all 0.5s;
+
+        }
+    }
+
+    
+    .nav-item:hover {
+                box-shadow: 5px 5px 10px #d0d5d8, -5px -5px 10px #ffffff;
+            }
+    @media (min-width: 640px) {
+        .nav-bar {
+            width: 640px;
+        }
+    }
+</style>
